119bc04f8459f5b8c955752af6d7042c764e23b1
rohit <rohitsirsat855@gmail.com>
2024-02-17 15:06:27 +0530

    fac

--- 03_loops/6_solution.py
+++ 03_loops/6_solution.py
@@ -1,9 +1,16 @@
 number = int(input("entere number : "))
+# number = 5
+
 factorial = 1
 
 
 while number > 0:
     factorial *= number
+    # factorial = factorial * number
     number -= 1
+    # number = number -1
 
 print("factorial value is", factorial)
+# output => 5*4*3*2*1 = 120
+
+
